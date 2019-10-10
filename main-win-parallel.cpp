f975595a59d9ac18c2230aea9ccebd940e229b53
Max Thyron <mkthyron@gmail.com>
2019-10-10 14:07:54 +0300

    file management

--- main-win-parallel.cpp
+++ main-win-parallel.cpp
@@ -1,22 +1,22 @@
 #include <opencv2/opencv.hpp>
-#include <cmath>
+#include <cmath> //
 #include <iostream>
 #include <string>
 #include <dirent.h>
-#include "cvui.h"
+#include <pthread.h>
 
 
 double faceTransform[6][2] =
         {
-                {0,         0},
+                {0, 0},
                 {M_PI / 2, 0},
-                {M_PI,      0},
+                {M_PI, 0},
                 {-M_PI / 2, 0},
-                {0,         -M_PI / 2},
-                {0,        M_PI / 2}
+                {0, -M_PI / 2},
+                {0, M_PI / 2}
         };
 
-std::unique_ptr<cv::Mat> getImage(const cv::String &path) {
+std::unique_ptr<cv::Mat> getImage(const cv::String& path) { // Do I need this
     std::unique_ptr<cv::Mat> image(new cv::Mat);
     *image = cv::imread(path);
     if ((*image).empty()) {
@@ -27,7 +27,7 @@ std::unique_ptr<cv::Mat> getImage(const cv::String &path) {
     return image;
 }
 
-inline void createCubeMapFace(const cv::Mat &in, cv::Mat &result) {
+inline void createCubeMapFace(const cv::Mat& in, cv::Mat& result) {
 
     double inWidth = in.cols;
     double inHeight = in.rows;
@@ -41,7 +41,7 @@ inline void createCubeMapFace(const cv::Mat &in, cv::Mat &result) {
     // Calculate adjacent (ak) and opposite (an) of the
     // triangle that is spanned from the sphere center
     //to our cube face.
-    const double an = sin(M_PI / 4);
+    const double an = sin(M_PI / 4); // Maybe use class for this
     const double ak = cos(M_PI / 4);
     double nx;
     double ny;
@@ -63,8 +63,8 @@ inline void createCubeMapFace(const cv::Mat &in, cv::Mat &result) {
             for (int x = 0; x < faceSize; x++) {
 
                 // Map face pixel coordinates to [-1, 1] on plane
-                nx = 2 * ((double) y / (double) faceSize - 0.5f);
-                ny = 2 * ((double) x / (double) faceSize - 0.5f);
+                nx = 2 * ((double)y / (double)faceSize - 0.5f);
+                ny = 2 * ((double)x / (double)faceSize - 0.5f);
 
 
                 // Map [-1, 1] plane coords to [-an, an]
@@ -79,12 +79,14 @@ inline void createCubeMapFace(const cv::Mat &in, cv::Mat &result) {
                     u = atan2(nx, ak);
                     v = atan2(ny * cos(u), ak);
                     u += ftu;
-                } else if (ftv > 0) {
+                }
+                else if (ftv > 0) {
                     // Bottom face
                     d = sqrt(nx * nx + ny * ny);
                     v = M_PI / 2 - atan2(d, ak);
                     u = atan2(ny, nx);
-                } else {
+                }
+                else {
                     // Top face
                     d = sqrt(nx * nx + ny * ny);
                     v = -M_PI / 2 + atan2(d, ak);
@@ -132,14 +134,16 @@ inline void createCubeMapFace(const cv::Mat &in, cv::Mat &result) {
         if (faceId < 4) {
             col = (faceId + 1) % 4;
             row = 1;
-        } else {
+        }
+        else {
             if (faceId == 4) {
-                fixRotate = cv::getRotationMatrix2D(cv::Point(face.rows / 2, face.cols / 2), -90, 1);
-            } else {
-                fixRotate = cv::getRotationMatrix2D(cv::Point(face.rows / 2 - 1, face.cols / 2), 90, 1);
+                fixRotate = cv::getRotationMatrix2D(cv::Point(face.rows / 2, face.cols / 2), -90, 1); // Get rid of this fix
+            }
+            else {
+                fixRotate = cv::getRotationMatrix2D(cv::Point(face.rows / 2 - 1, face.cols / 2), 90, 1); //
             }
 
-            cv::warpAffine(face, face, fixRotate, face.size());
+            cv::warpAffine(face, face, fixRotate, face.size()); //
             col = 1;
             row = (faceId % 4) * 2;
         }
@@ -148,9 +152,9 @@ inline void createCubeMapFace(const cv::Mat &in, cv::Mat &result) {
     }
 }
 
-void get_files(const std::string &dirPath, std::vector<std::string> &files) {
-    DIR *dir;
-    dirent *pdir;
+void get_files(const std::string &dirPath, std::vector<std::string>& files) { // TODO: Refactor - Find better solution
+    DIR* dir;
+    dirent* pdir;
 
     dir = opendir(dirPath.c_str());
     if (dir != nullptr) {
@@ -159,30 +163,62 @@ void get_files(const std::string &dirPath, std::vector<std::string> &files) {
                 files.emplace_back(pdir->d_name);
         }
         closedir(dir);
-    } else {
+    }
+    else {
         std::cout << "Directory not found!" << std::endl;
     }
 
 }
 
 
-int main(int argc, char **argv) {
+struct thread_data {
+    cv::Mat *result;
+    cv::Mat *source;
+    std::string file;
+};
+
+void *thread_function(void* threadarg) {
+    struct thread_data *data;
+    data = (struct thread_data *)threadarg;
+    std::string outputDir = R"(C:\Users\User\Desktop\Converter\Converter\x64\Release\output)";
+    createCubeMapFace(*(data->source), *(data->result));
+    cv::imwrite(outputDir + "\\" + data->file, *(data->result));
+    pthread_exit(nullptr);
+}
+
+#define NUM_THREADS 5
+
+
+int main(int argc, char** argv) {
     std::vector<std::string> files;
     std::unique_ptr<cv::Mat> source;
-    std::string path = R"(C:\Users\User\Desktop\Converter\Converter\x64\Release\images)";
-    std::string outputDir = R"(C:\Users\User\Desktop\Converter\Converter\x64\Release\output)";
-    cv::Mat *result;
+    std::string path = R"(C:\Users\User\Desktop\Converter\Converter\x64\Release\images)"; //  R"(C:\Users\User\Desktop\eq2cube-converter-master\cmake-build-debug\images)"; //
+    std::string outputDir = R"(C:\Users\User\Desktop\Converter\Converter\x64\Release\output)"; // R"(C:\Users\User\Desktop\eq2cube-converter-master\cmake-build-debug\output)"; //
+    cv::Mat* result;
 
     get_files(path, files);
+    int rc;
 
-    for (const std::string &file : files) {
-        std::cout << path + "\\" + file << "\n";
-        source = getImage(path + "\\" + file);
-        result = new cv::Mat((*source).rows * 3, (*source).rows * 4, (*source).type());
+    pthread_t threads[NUM_THREADS];
+    struct thread_data td[NUM_THREADS];
 
-        createCubeMapFace(*source, *result);
-        cv::imwrite(outputDir + "\\" + file, (*result));
+    int part_length = files.size() / NUM_THREADS;
+    std::cout << files.size() << " - " << part_length << std::endl;
+
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        int start = part_length * i;
+        int end = start + part_length;
+        for (int j = start; j < end; ++j) {
+            std::cout << path + "\\" + files[j] << "\n";
+            source = getImage(path + "\\" + files[j]);
+            result = new cv::Mat((*source).rows * 3, (*source).rows * 4, (*source).type());
+            td[i].result = result;
+            td[i].source = &(*source);
+            td[i].file = files[j];
+
+            rc = pthread_create(&threads[i], nullptr, thread_function, (void *) &td[i]);
+        }
     }
-
+    pthread_exit(nullptr);
     return 0;
 }
